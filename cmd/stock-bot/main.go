75a9cf3b9d7eb3c463c0dd1bea38646454c0e7e0
frankielearns <frankietruong987@gmail.com>
2019-08-12 23:38:57 -0400

    Main Bot code completed and is able to interact with Slack

--- cmd/stock-bot/main.go
+++ cmd/stock-bot/main.go
@@ -2,10 +2,69 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
   "github.com/piquette/finance-go/quote"
+	"github.com/nlopes/slack"
+  "strconv"
+  "regexp"
 )
 
-func main() {
-	// 15-min delayed full quote for Apple.
-	fmt.Println(quote.Get("XAW.TO"))
+func FloatToString(input_num float64) string {
+    // to convert a float number to a string
+    return strconv.FormatFloat(input_num, 'f', 6, 64)
 }
+
+func main() {
+
+	token := os.Getenv("SLACKTOKEN")
+	api := slack.New(token)
+
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+Loop:
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			fmt.Print("Event Received: ")
+			switch ev := msg.Data.(type) {
+			case *slack.ConnectedEvent:
+				fmt.Println("Connection counter:", ev.ConnectionCount)
+
+			case *slack.MessageEvent:
+				fmt.Printf("Message: %v\n", ev)
+				info := rtm.GetInfo()
+				prefix := fmt.Sprintf("<@%s> ", info.User.ID)
+
+				if ev.User != info.User.ID && strings.HasPrefix(ev.Text, prefix) {
+					respond(rtm, ev, prefix)
+				}
+
+			case *slack.RTMError:
+				fmt.Printf("Error: %s\n", ev.Error())
+
+			case *slack.InvalidAuthEvent:
+				fmt.Printf("Invalid credentials")
+				break Loop
+
+			default:
+				//Take no action
+			}
+		}
+	}
+}
+
+func respond(rtm *slack.RTM, msg *slack.MessageEvent, prefix string) {
+	var response string
+	text := msg.Text
+	text = strings.TrimPrefix(text, prefix)
+	text = strings.TrimSpace(text)
+	re := regexp.MustCompile(`<http://.*\||>`)
+	text = re.ReplaceAllString(text, " ")
+	stock, err := quote.Get(text)
+	if err == nil {
+						response = fmt.Sprintln("The stock", stock.ShortName, "is at", FloatToString(stock.RegularMarketPrice))
+						rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
+		}
+	}
